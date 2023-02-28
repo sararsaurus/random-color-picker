6b5f43aaebeca22c25bf5ea9d13986a626954f60
Sara Raley <sara.m.raley@gmail.com>
2023-02-28 14:11:01 -0700

    successfully changes button color on click

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,3 +1,16 @@
-export function Button(color, setColor) {
-  return <button>New color!</button>;
+import React, { useState } from "react";
+export function Button() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick} className={active ? "default" : "success"}>
+        New color!
+      </button>
+    </div>
+  );
 }
