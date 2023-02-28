125a31a348e3deb844525d2b086c35dfd893b9d4
Sara Raley <sara.m.raley@gmail.com>
2023-02-28 10:45:20 -0700

    adds rando.jsx code in old syntax

--- src/Random.jsx
+++ src/Random.jsx
@@ -1,9 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export function Random() {
-  return (
-    <div>
-      <h1>Welcome to React!</h1>
-    </div>
-  );
+// Random‘s job is to store a random color and to use that color to update the screen’s background.
+class Random extends React.Component {
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return "rgb(" + ary.join(", ") + ")";
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a, b) => a + b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random() * 256));
+    }
+    return random;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? "white" : "black"}></h1>
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(<Random />, document.getElementById("app"));
