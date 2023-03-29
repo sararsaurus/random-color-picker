078ad372b6f7edd0bbbdb8cc1a47c0369f69a981
Sara Raley <sara.m.raley@gmail.com>
2023-03-29 11:08:01 -0600

    starts random color generator

--- src/Button.jsx
+++ src/Button.jsx
@@ -6,6 +6,19 @@ export function Button() {
     setActive(!active);
   };
 
+  // random color generator
+  const randomColor = () => {
+    return (
+      "#" +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, "0")
+        .toUpperCase()
+    );
+  };
+
+  console.log(randomColor());
+
   return (
     <div>
       <button onClick={handleClick} className={active ? "default" : "success"}>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Button } from "./Button";
 
 export function Home() {
